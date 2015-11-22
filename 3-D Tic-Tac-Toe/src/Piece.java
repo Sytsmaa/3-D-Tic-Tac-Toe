70eb8d6191b1a6c0b497eeb8dc78548e3031baa2
Sytsmaa <sytsmaa@yahoo.com>
2015-11-21 20:54:55 -0500

    Updated Piece and Player
    Implemented them

--- 3-D Tic-Tac-Toe/src/Piece.java
+++ 3-D Tic-Tac-Toe/src/Piece.java
@@ -1,5 +1,21 @@
+import java.awt.Image;
 
+/**
+ * @author	Andrew Sytsma <asytsma@purdue.edu>
+ */
 public interface Piece
 {
-
-}
+	/**
+	 * Returns the picture of the piece.
+	 * 
+	 * @return	Returns the picture of the piece.
+	 */
+	public Image getPicture();
+	
+	/**
+	 * Returns the location of the piece.
+	 * 
+	 * @return	Returns the location of the piece.
+	 */
+	public Location getLocation();
+}	//end of Piece interface
--- 3-D Tic-Tac-Toe/src/Player.java
+++ 3-D Tic-Tac-Toe/src/Player.java
@@ -1,5 +1,12 @@
-
+/**
+ * @author	Andrew Sytsma <asytsma@purdue.edu>
+ */
 public interface Player
 {
-
-}
+	/**
+	 * Returns the username of the player.
+	 * 
+	 * @return	Returns the username of the player.
+	 */
+	public String getUsername();
+}	//end of Player interface
