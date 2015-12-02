82933d439289ff47395ea901f5d643ebdb9c506e
G-Rex2595 <grex2595@yahoo.com>
2015-12-02 02:35:21 -0500

    Started Web Development
    Started developing the website for the project.  I created a very basic
    layout, but it is subject to change.

---
+++ WebContent/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="design.css" rel="stylesheet" type="text/css" />
+<title>3D Tic-Tac-Toe</title>
+</head>
+
+<body>
+<div id="content-wrapper">
+	<div id="header">
+    	<h1>
+        	3D Tic-Tac-Toe
+        </h1>
+    </div>
+	<div id="inner-wrapper">
+    	<div id="left">
+    		<div class="nav">
+    			<a href="index.php">Home</a>
+    	    	<a href="index.php">Home</a>
+    	    	<a href="index.php">Home</a>
+    	    	<a href="index.php">Home</a>
+    	    	<a href="index.php">Home</a>
+    	    </div>
+		</div>
+		<div id="right">
+    		<div id="content">
+    	    	<p>Argh! This is where the content be.</p>
+    	    </div>
+		</div>
+    </div>
+    <div id="footer">
+    	&nbsp;
+    </div>
+</div>
+</body>
+</html>
