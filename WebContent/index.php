309dcb3c6a9c1f7faf59824a260fbe05c6d61c29
G-Rex2595 <grex2595@yahoo.com>
2015-12-02 12:49:40 -0500

    HTML5
    Made some changes to move to HTML5.

--- WebContent/index.php
+++ WebContent/index.php
@@ -1,8 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="design.css" rel="stylesheet" type="text/css" />
+<meta charset="utf-8" />
+<link rel="stylesheet" href="design.css" />
+
+<!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
 <title>3D Tic-Tac-Toe</title>
 </head>
 
