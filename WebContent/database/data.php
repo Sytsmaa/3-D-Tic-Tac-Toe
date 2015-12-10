2da93106b2c9ca5418c751ea793d3bcb8994ed5b
Sytsmaa <sytsmaa@yahoo.com>
2015-12-10 11:25:02 -0500

    We working mates
    Need to switch everything to DB2

--- WebContent/database/data.php
+++ WebContent/database/data.php
@@ -1,34 +1,4 @@
 <?php
-	//Referenced from https://hub.jazz.net/project/communitysample/php-mysql/overview#https://hub.jazz.net/git/communitysample%252Fphp-mysql/contents/master/index.php
-	echo '<h2>VCAP_SERVICE Environment variable</h2>';
-	echo "----------------------------------" . "</br>";
-	$key = "VCAP_SERVICES";
-	$value = getenv ( $key );
-	echo $key . ":" . $value . "</br>";
-	echo "----------------------------------" . "</br>";
-
-	/*$vcap_services = json_decode($_ENV["VCAP_SERVICES"]);
-	$db = $vcap_services->{'sqldb'}[0]->credentials;
-	$mysql_database = $vcap_services->{'sqldb'}[0]->name;
-	$mysql_server_name = $db->host . ':' . $db->port;
-	$mysql_username = $db->username;
-	$mysql_password = $db->password;
-
-	echo "name=" . $mysql_database . "\n";
-	echo "server=" . $mysql_server_name . "\n";
-	echo "username=" . $mysql_username . "\n";
-	echo "password=" . $mysql_password . "\n";
-
-	$mysql_server_name = "75.126.155.153:50000";
-	$mysql_username = "user11913";
-	$mysql_password = "CrCkhfK9hKDL";
-	$mysql_database = "SQL Database-eu";
-
-	$userData = mysql_connect($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
-
-	$conn_string = "DRIVER={IBM DB2 ODBC DRIVER};DATABASE=SQL Database-eu;HOSTNAME=75.126.155.153;PORT=50000;PROTOCOL=TCPIP;UID=user11913;PWD=CrCkhfK9hKDL";
-	$userData = db2_connect($conn_string, '', '');*/
-
 	//referenced from https://hub.jazz.net/project/ibmdatabase/dashDB/overview?cm_mc_uid=55561737454714477902675&cm_mc_sid_50200000=1449758628#https://hub.jazz.net/git/ibmdatabase%252FdashDB/contents/master/samples/dashDBPHP/index.php
 	$services_json = json_decode($json,true);
 	$blu = $services_json["sqldb"];
@@ -54,8 +24,6 @@
 
 	// connect to BLUDB
 	$userData = db2_connect($conn_string, '', '');
-
-	//$userData = mysqli_connect("75.126.155.153", "USER11358", "5S835bQMEcE1");//, "sql database-t8");
 	//$userData = mysqli_connect("localhost", "rayten", "password", "sql database-t8");
 	
 	if(!$userData)
