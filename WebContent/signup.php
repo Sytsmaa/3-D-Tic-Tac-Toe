7b7f23e33dba1445dd4f4bd02c4a5e947df3d5e0
G-Rex2595 <grex2595@yahoo.com>
2015-12-10 01:32:42 -0500

    Made Changes to Signup Database Insertion
    I had to make some changes to insertion on signup.  The problem is that
    the database is one of the stupidest things I've worked with.  We'll see
    if it even works.

--- WebContent/signup.php
+++ WebContent/signup.php
@@ -124,7 +124,8 @@
 			$hash = hashPassword($password);
 			
 			//create account
-			$sql = "INSERT INTO users (username, password, email) VALUES ('" . $username . "', '" . $hash . "', '" . $email . "')";
+			//$sql = "INSERT INTO users (username, password, email) VALUES ('" . $username . "', '" . $hash . "', '" . $email . "')";
+			$sql = "INSERT INTO users (username, password, email, casualWins, casualLosses, casualTies, easyWins, easyLosses, easyTies, mediumWins, mediumLosses, mediumTies, hardWins, hardLosses, hardTies, impossibleWins, impossibleLosses, impossibleTies) VALUES ('" . $username . "', '" . $hash . "', '" . $email . "', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')";
 			mysqli_query($userData, $sql);
 		
 			//set session variables
