1af2992723b071fbd4bf505b91f9eabbd4e9a6f9
G-Rex2595 <grex2595@yahoo.com>
2015-12-10 01:45:38 -0500

    Database Fixes 1
    There will probably be a lot of these.  I don't know why I can't connect
    to the database.

--- WebContent/database/data.php
+++ WebContent/database/data.php
@@ -1,5 +1,5 @@
 <?php
-	$userData = mysqli_connect("75.126.155.153", "user11358", "5S835bQMEcE1", "sql database-t8");
+	$userData = mysqli_connect("75.126.155.153", "USER11358", "5S835bQMEcE1");//, "sql database-t8");
 	//$userData = mysqli_connect("localhost", "rayten", "password", "sql database-t8");
 	
 	if(!$userData)
