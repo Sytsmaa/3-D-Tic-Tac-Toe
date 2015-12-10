eb4f3642310c53e10f02a7d8f161b1152a9533e8
G-Rex2595 <grex2595@yahoo.com>
2015-12-10 00:57:28 -0500

    Changed for Bluemix
    I forgot to change the code for Bluemix databases, but it is done now.

--- WebContent/database/data.php
+++ WebContent/database/data.php
@@ -1,6 +1,6 @@
 <?php
-	//$userData = mysqli_connect("75.126.155.153", "user11358", "5S835bQMEcE1", "sql database-t8");
-	$userData = mysqli_connect("localhost", "rayten", "password", "sql database-t8");
+	$userData = mysqli_connect("75.126.155.153", "user11358", "5S835bQMEcE1", "sql database-t8");
+	//$userData = mysqli_connect("localhost", "rayten", "password", "sql database-t8");
 	
 	if(!$userData)
 	{
