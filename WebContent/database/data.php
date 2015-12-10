58c3bb058fdbbf6bc71ef783f114fe825d6b6972
Sytsmaa <sytsmaa@yahoo.com>
2015-12-10 10:04:41 -0500

    team

--- WebContent/database/data.php
+++ WebContent/database/data.php
@@ -6,12 +6,18 @@
 	$value = getenv ( $key );
 	echo $key . ":" . $value . "</br>";
 	echo "----------------------------------" . "</br>";
+
 	$vcap_services = json_decode($_ENV["VCAP_SERVICES"]);
-	$db = $vcap_services->{'mysql-5.5'}[0]->credentials;
-	$mysql_database = $db->name;
+	$db = $vcap_services->{'sqldb'}[0]->credentials;
+	$mysql_database = $vcap_services->{'sqldb'}[0]->name;
 	$mysql_server_name = $db->host . ':' . $db->port;
 	$mysql_username = $db->username;
 	$mysql_password = $db->password;
+
+	echo "name=" . $mysql_database . "\n";
+	echo "server=" . $mysql_server_name . "\n";
+	echo "username=" . $mysql_username . "\n";
+	echo "password=" . $mysql_password . "\n";
 
 	$userData = mysql_connect($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
 	//$userData = mysqli_connect("75.126.155.153", "USER11358", "5S835bQMEcE1");//, "sql database-t8");
