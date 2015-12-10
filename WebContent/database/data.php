dc280c6cdaec6ba18a7e6646ab60ef28d3f64f17
Sytsmaa <sytsmaa@yahoo.com>
2015-12-10 10:52:03 -0500

    try db2

--- WebContent/database/data.php
+++ WebContent/database/data.php
@@ -17,19 +17,24 @@
 	echo "name=" . $mysql_database . "\n";
 	echo "server=" . $mysql_server_name . "\n";
 	echo "username=" . $mysql_username . "\n";
-	echo "password=" . $mysql_password . "\n";*/
+	echo "password=" . $mysql_password . "\n";
 
 	$mysql_server_name = "75.126.155.153:50000";
 	$mysql_username = "user11913";
 	$mysql_password = "CrCkhfK9hKDL";
 	$mysql_database = "SQL Database-eu";
 
-	$userData = mysql_connect($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
+	$userData = mysql_connect($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);*/
+
+	$conn_string = "DRIVER={IBM DB2 ODBC DRIVER};DATABASE=SQL Database-eu;HOSTNAME=75.126.155.153;PORT=50000;PROTOCOL=TCPIP;UID=user11913;PWD=CrCkhfK9hKDL";
+	$userData = db2_connect($conn_string, '', '');
+
 	//$userData = mysqli_connect("75.126.155.153", "USER11358", "5S835bQMEcE1");//, "sql database-t8");
 	//$userData = mysqli_connect("localhost", "rayten", "password", "sql database-t8");
 	
 	if(!$userData)
 	{
-		die("Connection failed: " . mysql_error());
+		//die("Connection failed: " . mysql_error());
+		die("Connection failed: " . db2_conn_error());
 	}
 ?>
