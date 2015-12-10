c40eb5303cc0bc6f4f708470a281dcefebbe0d8d
G-Rex2595 <grex2595@yahoo.com>
2015-12-10 02:09:14 -0500

    Database Fixes 2
    It's late, but I just figured out what my biggest issue with doing the
    databasing is.  I'll make it work tomorrow after I've had more time to
    look at it.

--- WebContent/database/data.php
+++ WebContent/database/data.php
@@ -1,6 +1,26 @@
 <?php
-	$userData = mysqli_connect("75.126.155.153", "USER11358", "5S835bQMEcE1");//, "sql database-t8");
+	$userData = db2_connect("75.126.155.153", "USER11358", "5S835bQMEcE1");//, "sql database-t8");
 	//$userData = mysqli_connect("localhost", "rayten", "password", "sql database-t8");
+	
+	function query($sqlQuery)
+	{
+		$prepared = db2_prepare($userdata,$sqlQuery);
+		db2_execute($prepared);
+		return $prepared;
+		//return mysqli_query($userData, $sqlQuery);
+	}
+	
+	function nextRow($queryResult)
+	{
+		return db2_fetch_row($queryResult);
+		//return mysqli_fetch_assoc($queryResult);
+	}
+	
+	function close()
+	{
+		db2_close($userData);
+		//mysqli_close($userdata);
+	}
 	
 	if(!$userData)
 	{
--- WebContent/layout/footer.php
+++ WebContent/layout/footer.php
@@ -28,5 +28,5 @@
 </body>
 </html>
 <?php
-	mysqli_close($userData);
+	close();
 ?>
